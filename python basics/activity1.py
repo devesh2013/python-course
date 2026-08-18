0d3aa11fe37eea15108abcc81134f93d98d2067c
devesh2013 <yogeshkrdaga@gmail.com>
2026-08-18 20:14:46 +0300

    x and y

---
+++ python basics/activity1.py
@@ -0,0 +1,10 @@
+#print a sentance
+print("Hi,my name is Devesh")
+#printing a number
+print(13)
+#making a new line
+print("i love coding \n")
+#passing a argumant with a string
+print("Hi i am ",13)
+#printing the end argumant
+print("welcome to ",end="codingal")
---
+++ python basics/activity2.py
@@ -0,0 +1,9 @@
+x = "Hi"
+y = "my name is Devesh"
+print(x)
+print(y)
+codingal = "codingal"
+print("codingal")
+# printing a name
+name = input("enter ur name")
+print("\nHi" , name, "\nWelcome to school")
