cc19d75cedca01a76dd0af910c48b5925f30e7c0
devesh2013 <yogeshkrdaga@gmail.com>
2026-08-18 16:31:24 +0300

    my routine

---
+++ homework/rountine.py
@@ -0,0 +1,2 @@
+print("hi my name is Devesh daga.the way i always stay active is that I wake up in the morning,get fresh and start studying.After some time I eat my breakfast and play games in my ipad.Then i go back to studying,doing my clases then i go out to play badminton. After I come back from playing I take a bath again,eat my dinner and go back to sleep")
+
