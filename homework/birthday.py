566be8da1293b87bc6dde2505506f90df640ad48
devesh2013 <yogeshkrdaga@gmail.com>
2026-08-21 18:50:39 +0300

    birthday

---
+++ homework/birthday.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+month = input("What month is your birthday? ")
+day = input("What day is your birthday? ")
+
+print("\n Birthday chart")
+print("Name:", name)
+print("Birthday:", month, day)
