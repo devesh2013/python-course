f497614f10629a6faddd515ce3073eeb0725b01b
devesh2013 <yogeshkrdaga@gmail.com>
2026-09-08 20:22:47 +0300

    test

---
+++ python basics/conditionals.py
@@ -0,0 +1,15 @@
+
+print("which is the IPL team") 
+print("a.CSK") 
+print("b.RR") 
+print("c.RCB") 
+print("d.KK") 
+team = str(input("enter the answer as a/b/c/d"))
+if team == "a" :
+    print("a is not the correct answer")
+elif team == "b" :
+    print("b is not the correct answer")
+elif team == "c" :
+    print("c is the correct answer")
+else:
+    print("d is not the correct answer")
