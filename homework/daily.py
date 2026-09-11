b04191b090737990474d12c3fbe1dcd26da49ffb
devesh2013 <yogeshkrdaga@gmail.com>
2026-09-11 11:39:57 +0300

    rountine

---
+++ homework/daily.py
@@ -0,0 +1,14 @@
+print("what do u want to do?") 
+print("a.do homework") 
+print("b.play games") 
+print("c.play outside") 
+print("d.sleep for 2 months") 
+routine = str(input("enter the answer as a/b/c/d"))
+if routine == "a" :
+    print("a is not the correct answer")
+elif routine == "b" :
+    print("b is not the correct answer")
+elif routine == "c" :
+    print("c is not the correct answer")
+else:
+    print("d is the correct answer")
